2f506903f9ab11cb4ddd61c396c5ab97584f1813
Evans Mwendwa <evansmwendwa@users.noreply.github.com>
2018-06-20 03:20:13 +0300

    initial project setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="full-width center-content">
+    <img src="/static/img/logo.png" alt="" />
+    <hello-component name="World" />
+  </div>
+</template>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #f0f;
+  font-weight: 300;
+  text-transform: uppercase;
+  font-size: 62px;
+}
+</style>
