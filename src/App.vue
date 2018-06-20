fe3a08d3e3e9000f3dc7b51372a6f9676363da27
Evans Mwendwa <evansmwendwa@users.noreply.github.com>
2018-06-20 15:44:38 +0300

    added taskmanagement

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="container">
     <h3>{{ title }}</h3>
+    <form>
+      <textarea placeholder="Add new task" v-model="newTask"></textarea>
+      <p>{{newTask}}</p>
+
+      <button type="button" v-on:click="addTask">Add Task {{tasks.length}}</button>
+
+    </form>
+    <ul>
+      <li v-for="task in tasks">{{task}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+
 export default {
   data: function() {
     return {
-      title: 'My App'
+      title: 'My Task Manager',
+      newTask: 'This is a SAMPLE ',
+      tasks: []
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.unshift(this.newTask);
+      this.newTask = '';
     }
   }
 }
